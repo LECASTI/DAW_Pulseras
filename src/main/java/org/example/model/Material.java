aef9e020944638e8a5b37b7de8bc31fff65f9551
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-04-29 20:18:15 -0600

    Update Material.java
    Añadido tamaño, stock y ruta imagen

--- src/main/java/org/example/model/Material.java
+++ src/main/java/org/example/model/Material.java
@@ -10,6 +10,10 @@ public class Material implements Serializable {
     private String nombre;
     private String descripcion;
     private String tipo;
+    private ObjectId color_id;
+    private int tamano_mm;
+    private int cantidad_inventario;
+    private String ruta_imagen; // Esto puede ser ruta relativa, directa o URL
 
     public Material() {}
 
@@ -22,5 +26,12 @@ public class Material implements Serializable {
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
     public String getTipo() { return tipo; }
     public void setTipo(String tipo) { this.tipo = tipo; }
-}
+    public ObjectId getColorId() { return color_id; }
+    public void setColorId(ObjectId id) { this.color_id = id; }
+    public int getTamano() { return this.tamano_mm; }
+    public void setTamano(int tamano) { this.tamano_mm = tamano; }
+    public int getCantidad() { return this.cantidad_inventario; }
+    public void setCantidad(int stock) {this.cantidad_inventario = stock; }
+    public String getRutaImagen() { return this.ruta_imagen; }
+    public void setRutaImagen(String ruta) { this.ruta_imagen = ruta; }
 //===== Fin Material =====//
