f4536bd0890a1bd4f41876bde557ac66c42bf3ea
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-04-29 21:05:24 -0600

    Update Builds.java
    Eliminada referencia a usuario

--- src/main/java/org/example/model/Builds.java
+++ src/main/java/org/example/model/Builds.java
@@ -8,7 +8,6 @@ import org.bson.types.ObjectId;
 //===== Bean Builds =====//
 public class Builds implements Serializable {
     private ObjectId id;
-    private ObjectId usuarioId;    // Obligatorio (solo usuarios registrados)
     private List<ObjectId> pulserasIds;
 
     public Builds() {}
@@ -16,8 +15,6 @@ public class Builds implements Serializable {
     // Getters y setters
     public ObjectId getId() { return id; }
     public void setId(ObjectId id) { this.id = id; }
-    public ObjectId getUsuarioId() { return usuarioId; }
-    public void setUsuarioId(ObjectId usuarioId) { this.usuarioId = usuarioId; }
     public List<ObjectId> getPulserasIds() { return pulserasIds; }
     public void setPulserasIds(List<ObjectId> pulserasIds) { this.pulserasIds = pulserasIds; }
 }
