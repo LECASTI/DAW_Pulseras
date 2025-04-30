913ab5b073a9d1687cadfa55329300ad6ac28166
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-04-29 20:46:50 -0600

    Update Pulsera.java
    Añadido builds y descripcion. Eliminado repairCount (se mueve a Ventas.java

--- src/main/java/org/example/model/Pulsera.java
+++ src/main/java/org/example/model/Pulsera.java
@@ -8,18 +8,21 @@ import org.bson.types.ObjectId;
 //===== Bean Pulsera =====//
 public class Pulsera implements Serializable {
     private ObjectId id;
+    private String descripcion
     private Double circunferencia;
     private Double precio;
     private List<ObjectId> materialesIds;
     private List<ObjectId> coloresIds;
-    private Integer repairCount;
     private Boolean delisted;
+    private Boolean userBuilt; //Falso == creado por admin
 
     public Pulsera() {}
 
     // Getters y setters
     public ObjectId getId() { return id; }
     public void setId(ObjectId id) { this.id = id; }
+    public String getDescripcion() {return this.descripcion}
+    public void setDescripcion(String descripcion) {this.descripcion = descripcion}
     public Double getCircunferencia() { return circunferencia; }
     public void setCircunferencia(Double circunferencia) { this.circunferencia = circunferencia; }
     public Double getPrecio() { return precio; }
@@ -28,8 +31,6 @@ public class Pulsera implements Serializable {
     public void setMaterialesIds(List<ObjectId> materialesIds) { this.materialesIds = materialesIds; }
     public List<ObjectId> getColoresIds() { return coloresIds; }
     public void setColoresIds(List<ObjectId> coloresIds) { this.coloresIds = coloresIds; }
-    public Integer getRepairCount() { return repairCount; }
-    public void setRepairCount(Integer repairCount) { this.repairCount = repairCount; }
     public Boolean getDelisted() { return delisted; }
     public void setDelisted(Boolean delisted) { this.delisted = delisted; }
 }
