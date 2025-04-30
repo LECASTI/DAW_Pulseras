a274387c01e8e6bc42e1648e089fa649e48308bd
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-04-29 20:54:12 -0600

    Create Ventas.java

---
+++ src/main/java/org/example/model/Ventas.java
@@ -0,0 +1,23 @@
+// src/main/java/org/example/model/Ventas.java
+package org.example.model;
+
+import java.io.Serializable;
+import java.util.List;
+import org.bson.types.ObjectId;
+
+//===== Bean Ventas =====//
+public class Builds implements Serializable {
+    private ObjectId id;
+    private ObjectId usuarioId;    // Obligatorio (solo usuarios registrados)
+    private ObjectId pulseraId;
+    public Ventas() {}
+
+    // Getters y setters
+    public ObjectId getId() { return id; }
+    public void setId(ObjectId id) { this.id = id; }
+    public ObjectId getUsuarioId() { return usuarioId; }
+    public void setUsuarioId(ObjectId usuarioId) { this.usuarioId = usuarioId; }
+    public ObjectId getPulseraId() { return this.pulseraId; }
+    public void setPulseraId(ObjectId id) { this.pulseraId = id; }
+}
+//===== Fin Ventas =====//
