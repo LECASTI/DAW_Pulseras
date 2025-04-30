c98a70482b5a69673233208a7316d1079f72f0fe
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-04-29 20:34:11 -0600

    Create Builds.java
    Lo estuve analizando y pues es exactamente igual a Favoritos xd

---
+++ src/main/java/org/example/model/Builds.java
@@ -0,0 +1,24 @@
+// src/main/java/org/example/model/Builds.java
+package org.example.model;
+
+import java.io.Serializable;
+import java.util.List;
+import org.bson.types.ObjectId;
+
+//===== Bean Builds =====//
+public class Builds implements Serializable {
+    private ObjectId id;
+    private ObjectId usuarioId;    // Obligatorio (solo usuarios registrados)
+    private List<ObjectId> pulserasIds;
+
+    public Favoritos() {}
+
+    // Getters y setters
+    public ObjectId getId() { return id; }
+    public void setId(ObjectId id) { this.id = id; }
+    public ObjectId getUsuarioId() { return usuarioId; }
+    public void setUsuarioId(ObjectId usuarioId) { this.usuarioId = usuarioId; }
+    public List<ObjectId> getPulserasIds() { return pulserasIds; }
+    public void setPulserasIds(List<ObjectId> pulserasIds) { this.pulserasIds = pulserasIds; }
+}
+//===== Fin Builds =====//
